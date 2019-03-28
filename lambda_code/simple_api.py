384daaf1729fa6f083044d7b1f5fc263f6f75e17
Boyd, Richard <rboyd@irobot.com>
2019-03-27 21:57:33 -0400

    updated request builder

--- lambda_code/simple_api.py
+++ lambda_code/simple_api.py
@@ -3,8 +3,26 @@ import os
 from botocore.model import ServiceModel
 from botocore.loaders import Loader
 from botocore.serialize import create_serializer
+from botocore.parsers import create_parser
+from botocore.awsrequest import AWSRequest
+from botocore import session
 
 REGION = os.environ['AWS_REGION']
+
+
+def get_endpoint(service_name):
+    this_session = session.get_session()
+    client = this_session.create_client(service_name, region_name=REGION)
+    return client.meta.endpoint_url
+
+
+def create_request():
+    pass
+
+
+def create_response():
+    pass
+
 
 def integration_template():
     return {
@@ -16,6 +34,8 @@ def integration_template():
                     "application/json": ''
                 },
             },
+            {"StatusCode": 400, "SelectionPattern": "4[0-9]{2}"},
+            {"StatusCode": 500, "SelectionPattern": "5[0-9]{2}"}
         ],
         "PassthroughBehavior": "WHEN_NO_MATCH",
         "RequestParameters": {},
@@ -45,22 +65,48 @@ def handle_method(fragment):
     loader = Loader()
     service_description = loader.load_service_model(service_name=service_name, type_name='service-2')
     service_model = ServiceModel(service_description)
+    protocol = service_model.protocol
     op_model = service_model.operation_model(action["Name"])
+
     request_parameters = action["Parameters"]
     params = dict(ChainMap(*request_parameters))
-    serializer = create_serializer(service_model.protocol)
+    print("params: {}".format(params))
+    serializer = create_serializer(protocol)
+    response_parser = create_parser(protocol)
+
+    print(service_model.protocol)
     request = serializer.serialize_to_request(params, op_model)
-    print("Request: {}".format(request))
+    request_object = AWSRequest(
+        method=request['method'],
+        url=get_endpoint(service_model.service_name),
+        data=request['body'],
+        headers=request['headers'])
+
+    X = request_object.prepare()
+
+    print("Raw request: {}".format(request))
+    print("Prepared request: {}".format(X))
 
     integration = fragment["Properties"]["Integration"]
     new_integration = integration_template()
 
+    # Copy the existing values to the new template
     for entry in integration.keys():
         new_integration[entry] = integration[entry]
 
-    for header in request['headers'].keys():
-        new_integration["RequestParameters"].update({"integration.request.header.{}".format(header): "'{}'".format(request['headers'][header])})
-    new_integration["RequestTemplates"]["application/json"] = request["body"].decode("utf-8")
+    # Add headers to cfn template
+    if X.headers is not None and callable(getattr(X.headers, "keys", None)):
+        for header in X.headers.keys():
+            if header.lower() != 'Content-Length'.lower():
+                new_integration["RequestParameters"].update({"integration.request.header.{}".format(header): "'{}'".format(X.headers[header])})
+
+    # Add Query Strings to cfn template
+    if 'query_string' in request and callable(getattr(request['query_string'], "keys", None)):
+        for query in request['query_string'].keys():
+            new_integration["RequestParameters"].update({"integration.request.querystring.{}".format(query): "'{}'".format(request['query_string'][query])})
+
+    # Set the body
+    new_integration["RequestTemplates"]["application/json"] = str(X.body, "utf-8") if X.body else ''
     new_integration["Uri"] = ":".join([
         "arn",
         "aws",
@@ -69,7 +115,7 @@ def handle_method(fragment):
         service_model.endpoint_prefix,
         "path/" + request["url_path"]
     ])
-    new_integration["IntegrationHttpMethod"] = request["method"]
+    new_integration["IntegrationHttpMethod"] = X.method
 
     fragment["Properties"]["Integration"] = new_integration
     print(fragment)
@@ -77,11 +123,11 @@ def handle_method(fragment):
 
 
 def lambda_handler(event, _context):
-    # print("event: {}".format(event))
     status = "success"
     fragment = event["fragment"]
     try:
         fragment = handle_method(fragment)
+        print("transformed fragment: {}".format(fragment))
     except InvalidTypeException as e:
         print("Invalid type supplied: {}".format(e))
         status = "failure"
